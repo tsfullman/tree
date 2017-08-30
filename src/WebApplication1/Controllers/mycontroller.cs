c5bc68ff4a9646f0dfb2ccfa206ef44becb3c387
Trevor Fullman <tsfullman@verizon.net>
2017-08-30 12:33:31 -0400

    Added view for Detail

--- src/WebApplication1/Controllers/mycontroller.cs
+++ src/WebApplication1/Controllers/mycontroller.cs
@@ -10,11 +10,7 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek==DayOfWeek.Tuesday)
-            {
-                return Redirect("/");
-            }
-            return Content("Hello from my controller");
+            return View();
         }
     }
 }
