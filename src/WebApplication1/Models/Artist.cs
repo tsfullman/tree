44cea8e04cd0bcd074da9f986ee7447cf340fd52
Trevor Fullman <tsfullman@verizon.net>
2017-08-30 20:54:31 -0400

    Added model1 and artist models

---
+++ src/WebApplication1/Models/Artist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Artist
+    {
+
+        public string Name { get; set; }
+        public String Role { get; set; }
+    }
+}
