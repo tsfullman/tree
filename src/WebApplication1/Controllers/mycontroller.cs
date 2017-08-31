6eb454aa0b05736511863f9a798c171e65123033
Trevor Fullman <tsfullman@verizon.net>
2017-08-30 21:33:58 -0400

    Made comicbook detail view strongly typed

--- src/WebApplication1/Controllers/mycontroller.cs
+++ src/WebApplication1/Controllers/mycontroller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -10,20 +11,22 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Detail()
         {
-            ViewBag.Series = "The loser Spider Man";
-            ViewBag.Issuenumber = 700;
-            ViewBag.description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-
-            ViewBag.Artists = new String[] {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
-        };
-
-
-            return View();
+            var comicBook = new model1()
+            {
+                SeriesTitle = "The loser Spider Man",
+                IssueNumber=700,
+                DescriptionHtml = "<p> Final issue!Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists=new Artist[]
+                {
+                    new Artist() {Name="Dan Slott",Role="Script"},
+                    new Artist() {Name="Humberto Ramos",Role="Pencils"},
+                    new Artist() {Name="Victor Olazaba",Role="inks"},
+                    new Artist() {Name="Edgar Delgado",Role="Colors"},
+                    new Artist() {Name="Chris Bosh",Role="Letters"},
+                }
+            };
+        
+            return View(comicBook);
         }
     }
 }
--- src/WebApplication1/Models/model1.cs
+++ src/WebApplication1/Models/model1.cs
@@ -13,5 +13,21 @@ namespace WebApplication1.Models
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
     }
 }
