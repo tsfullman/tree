a8f41f4ef2c9d72298311a84552f579db7beb3ec
Trevor Fullman <tsfullman@verizon.net>
2017-09-05 16:21:57 -0400

    Began football app

---
+++ src/FootballApp/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FootballApp.Models;
+
+namespace FootballApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private FootballdatabaseEntities _db = new FootballdatabaseEntities();
+
+        public ActionResult Index()
+        {
+            return View(_db.FootballTeamsTables.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
